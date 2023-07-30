fee489338d595fc9f0b8aabd2ae9fa9f2e2fda81
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2023-07-31 01:54:18 +0700

    hbn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as pi from "./math.js";
 
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
